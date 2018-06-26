c2ad439a56b9a276fb9dd2c1bb261d221da87c98
Lydia Rennert <lydia.rennert@gmail.com>
2018-06-26 19:34:13 +0200

    Hide empty description field

--- public/javascripts/utils/handlebarsHelpers.js
+++ public/javascripts/utils/handlebarsHelpers.js
@@ -1,3 +1,7 @@
 Handlebars.registerHelper("ifStateOpen", function (condition, options) {
     return condition === "open" ? options.fn(this) : options.inverse(this);
 });
+
+Handlebars.registerHelper("ifDescriptionExists", function (condition, options) {
+    return condition !== "" ? options.fn(this) : options.inverse(this);
+});
