0e7d09c2cc6524ae2a91dac3f39889e3d2369f92
Lydia Rennert <lydia.rennert@gmail.com>
2018-06-27 12:40:08 +0200

    Remove unused modules

--- app.mjs
+++ app.mjs
@@ -4,15 +4,15 @@ import bodyParser from 'body-parser';
 
 import {noteRouter} from './routes/note-router';
 
+const app = express();
+
 // workaround for __dirname not available with --experimental-modules
 // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-const app = express();
-
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(bodyParser.json());
+
 app.use("/", noteRouter);
 
 export default app;
--- routes/note-router.mjs
+++ routes/note-router.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import {noteController} from '../controllers/noteController';
+import {noteController} from '../controllers/note-controller';
 
 const router = express.Router();
 
